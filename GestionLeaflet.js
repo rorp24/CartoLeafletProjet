346a30cda18fbb05bf3bd8ed3df0f6dc3dfdea13
Unknown <robinelzeard@gmail.com>
2019-05-19 19:41:20 +0200

    commencement des tests

--- GestionLeaflet.js
+++ GestionLeaflet.js
@@ -1,1 +1,6 @@
 var map = L.map('map').fitWorld();
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
