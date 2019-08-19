68348885ba378924c55163520392550552854d1d
Unknown <robinelzeard@gmail.com>
2019-08-19 14:50:31 +0200

    update pour get les deux JSON

--- map.js
+++ map.js
@@ -1,21 +1,28 @@
 var monJson
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        monJson = JSON.parse(this.responseText);
-        console.log("Json récupéré:", monJson)
-        document.getElementById("description").innerHTML = monJson.description;
-    }
-};
-
 
 function EnterThePage() {
     if (window.location.search == "?GBN") {
         alert("Grenoble by night")
-            //open("demo.json")
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                monJson = JSON.parse(this.responseText);
+                console.log("Json récupéré:", monJson)
+                document.getElementById("description").innerHTML = monJson.description;
+            }
+            xmlhttp.open("GET", "./GrenobleByNight.json", true);
+            xmlhttp.send();
+        }
     } else if (window.location.search == "?demo") {
         alert("demo")
-        xmlhttp.open("GET", "./demo.json", true);
-        xmlhttp.send();
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                monJson = JSON.parse(this.responseText);
+                console.log("Json récupéré:", monJson)
+                document.getElementById("description").innerHTML = monJson.description;
+            }
+            xmlhttp.open("GET", "./demo.json", true);
+            xmlhttp.send();
+        }
     }
 }
