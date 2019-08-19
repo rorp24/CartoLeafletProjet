6829c75f4ac83ce79818a56a7e38513c409adf76
Unknown <robinelzeard@gmail.com>
2019-08-19 14:40:13 +0200

    début lecture json

--- map.js
+++ map.js
@@ -1,9 +1,20 @@
 var monJson
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        monJson = JSON.parse(this.responseText);
+        document.getElementById("description").innerHTML = monJson.description;
+    }
+};
 
 function EnterThePage() {
     if (window.location.search == "?GBN") {
         alert("Grenoble by night")
+            //open("demo.json")
     } else if (window.location.search == "?demo") {
         alert("demo")
+
+        xmlhttp.open("GET", "./demo.json", true);
+        xmlhttp.send();
     }
 }
