8f2aedf7968c46a5b4cba27fa74ab16cd55cea47
Unknown <robinelzeard@gmail.com>
2019-08-19 14:17:04 +0200

    sélection de ce qu'on va load pour l'utilisateur

--- map.js
+++ map.js
@@ -1,3 +1,9 @@
-function EnterThePage() {
+var monJson
 
+function EnterThePage() {
+    if (window.location.search == "?GBN") {
+        alert("Grenoble by night")
+    } else if (window.location.search == "?demo") {
+        alert("demo")
+    }
 }
