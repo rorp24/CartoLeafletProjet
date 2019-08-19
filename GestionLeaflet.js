5f7ad1c259b33105b0306ad193bc79762166d181
Unknown <robinelzeard@gmail.com>
2019-08-19 15:02:55 +0200

    gestion centre + delete gestionLeaflet (car inutile)

--- GestionLeaflet.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map').fitWorld();
-
-L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-	maxZoom: 20,
-	attribution: '&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.control.scale().addTo(this.map)
--- map.js
+++ map.js
@@ -1,31 +1,40 @@
 var monJson
+var map = L.map('map').fitWorld();
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.control.scale().addTo(map)
 
 function EnterThePage() {
     if (window.location.search == "?GBN") {
-        alert("Grenoble by night")
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                monJson = JSON.parse(this.responseText);
-                console.log("Json récupéré:", monJson)
-                document.getElementById("description").innerHTML = monJson.description;
+                useJSON(this.responseText)
             }
 
         }
         xmlhttp.open("GET", "./GrenobleByNight.json", true);
         xmlhttp.send();
     } else if (window.location.search == "?demo") {
-        alert("demo")
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                monJson = JSON.parse(this.responseText);
-                console.log("Json récupéré:", monJson)
-                document.getElementById("description").innerHTML = monJson.description;
+                useJSON(this.responseText)
             }
 
         }
         xmlhttp.open("GET", "./demo.json", true);
         xmlhttp.send();
     }
+}
+
+function useJSON(text) {
+    monJson = JSON.parse(text);
+    console.log("Json récupéré:", monJson)
+    document.getElementById("description").innerHTML = monJson.description;
+    map.setView(monJson.center, monJson.zoom)
 }
