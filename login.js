ac0726cebc9332b1002a9ec6e11af978572ed6b5
Unknown <robinelzeard@gmail.com>
2019-09-02 14:20:21 +0200

    login

---
+++ login.js
@@ -0,0 +1,28 @@
+document.getElementById("login").innerHTML = '<div id="notLoged"><button onclick="login()">Log in</button>   <input type="email" id="username"></input>   <input type="password" id="password"></input></div><div id="loged" hidden><button>Log out</button></div>'
+document.head.innerHTML = document.head.innerHTML + '<link rel="stylesheet" href="login.css" />'
+
+var user = firebase.auth().currentUser;
+
+if (user) {
+    // User is signed in.
+    document.getElementById('loged').removeAttribute("hidden");
+    document.getElementById('notLoged').setAttribute("hidden", true);
+} else {
+    // No user is signed in.
+}
+
+function login() {
+    console.log("login cliqué")
+    var email = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    console.log("email:" + email)
+    console.log("password:" + password)
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.error("error " + errorCode + ":" + errorMessage)
+            // ...
+    });
+}
